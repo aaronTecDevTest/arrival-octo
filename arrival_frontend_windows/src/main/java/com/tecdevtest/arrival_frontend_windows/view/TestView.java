5d76e8c4b9e973a2b8b3f668ad0471012b98b9d4
aarontecdevtest <pichou.kutekidila@gmail.com>
2015-03-28 22:36:54 +0100

    Projektstrucktur

---
+++ arrival_frontend_windows/src/main/java/com/tecdevtest/arrival_frontend_windows/view/TestView.java
@@ -0,0 +1,7 @@
+package com.tecdevtest.arrival_frontend_windows.view;
+
+/**
+ * Created by Aaron on 28.03.2015.
+ */
+public class TestView {
+}
---
+++ src/main/java/RunArrivalOcto.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Aaron on 28.03.2015.
+ * @author Aaron K.
+ * @versio 0.0.1
+ */
+
+import java.io.*;
+
+public class RunArrivalOcto {
+    public static void main(String[] args) {
+        System.out.println("Hallo ArrivalOcto");
+    }
+}
